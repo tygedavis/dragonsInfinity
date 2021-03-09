3988083e6ded8d8a9ca3e10bc267f4ba760ff620
tygedavis <tdavis@cmsonline.com>
2021-03-09 14:53:49 -0700

    Added file for PlayerAccountModal and made a function for toggling the modal in the Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Button from '../Button/Button';
 import './Header.css';
 
 export default function Header() {
+
+    const [playerAccountModal, setPlayerAccountModal] = useState(false);
+
+    function togglePlayerAccountModal(){
+        setPlayerAccountModal(!playerAccountModal)
+    }
+
     return(
         <div className='header'>
             <h1>Character Name: <span className='character-name'>Arminius</span></h1>
             <div className='header-buttons'>
                 <Button
                     text='Player Account'
+                    onClick={togglePlayerAccountModal}
                 />
                 <Button
                     text='Character Info'
---
+++ src/playerAccount/PlayerAccountModal.js
@@ -0,0 +1,9 @@
+
+
+export default function PlayerAccountModal() {
+    return (
+        <div>
+
+        </div>
+    );
+};
